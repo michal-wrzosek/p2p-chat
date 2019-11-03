f4080ea40fac9005c808334014d3c93d46cc9b8b
Michał Wrzosek <michal@wrzosek.pl>
2019-11-03 10:09:36 +0100

    Updates

---
+++ precache-manifest.8d898ddc7debf4afe1c992c70c7bb524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60c1431c6e8897f592634355f15da6f",
+    "url": "/p2p-chat/index.html"
+  },
+  {
+    "revision": "bfdaba6ff17d7286ddd4",
+    "url": "/p2p-chat/static/css/2.e3f77e86.chunk.css"
+  },
+  {
+    "revision": "76161be2643284ac1c27",
+    "url": "/p2p-chat/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "bfdaba6ff17d7286ddd4",
+    "url": "/p2p-chat/static/js/2.1729d825.chunk.js"
+  },
+  {
+    "revision": "76161be2643284ac1c27",
+    "url": "/p2p-chat/static/js/main.91fbe544.chunk.js"
+  },
+  {
+    "revision": "b4a4b98981acf000c6e7",
+    "url": "/p2p-chat/static/js/runtime-main.a6d0ffa9.js"
+  }
+]);
