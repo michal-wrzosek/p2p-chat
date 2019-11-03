93530e44681b9aa1ad6d1c0bcfe4531e21ecb030
Michał Wrzosek <michal@wrzosek.pl>
2019-11-03 14:03:08 +0100

    Experimenting with STUN/TURN

--- src/createPeerConnection.ts
+++ src/createPeerConnection.ts
@@ -20,13 +20,12 @@ export function createPeerConnection({
   const peerConnection = new RTCPeerConnection({
     iceServers: [
       {
-        urls: [
-          'stun:stun.l.google.com:19302',
-          'stun:stun1.l.google.com:19302',
-          'stun:stun2.l.google.com:19302',
-          'stun:stun3.l.google.com:19302',
-          'stun:stun4.l.google.com:19302',
-        ],
+        urls: 'stun:stun.l.google.com:19302',
+      },
+      {
+        urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
+        username: 'webrtc',
+        credential: 'webrtc',
       },
     ],
   });
